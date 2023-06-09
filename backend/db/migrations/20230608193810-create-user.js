ea14e64dc4694da771d50955c0a3b793ff9a0b8a
Daniel Lewis <daniel.c.lewis@outlook.com>
2023-06-09 12:44:44 -0500

    Undo options changes in migration and seeder files

--- backend/db/migrations/20230608193810-create-user.js
+++ backend/db/migrations/20230608193810-create-user.js
@@ -1,8 +1,6 @@
 'use strict';
 
-let options = {
-  schema: "api_schema"
-};
+let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
