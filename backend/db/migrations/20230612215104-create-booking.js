6beaab6af8ee2b20862bcf5699c7c3807dee7c41
Daniel Lewis <daniel.c.lewis@outlook.com>
2023-06-12 18:37:48 -0500

    Updating options and belongsTo relationship order

--- backend/db/migrations/20230612215104-create-booking.js
+++ backend/db/migrations/20230612215104-create-booking.js
@@ -1,4 +1,10 @@
 'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -41,9 +47,9 @@ module.exports = {
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Bookings');
+    await queryInterface.dropTable('Bookings', options);
   }
 };
