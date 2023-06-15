5a5e138f005fcdad716713aa48346e65c42fbc27
Daniel Lewis <daniel.c.lewis@outlook.com>
2023-06-15 14:20:23 -0500

    Added GET ALL REVIEWS OF THE CURRENT USER endpoint.

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,16 +4,17 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
+const reviewRouter = require('./reviews.js');
 
 const { requireAuth, restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
-  // If current user session is valid, set req.user to the user in the database
-  // If current user session is not valid, set req.user to null
+// If current user session is valid, set req.user to the user in the database
+// If current user session is not valid, set req.user to null
 router.use(restoreUser);
 
 router.get('/test', requireAuth, (req, res) => {
-  res.json({message: 'success'})
+  res.json({ message: 'success' });
 });
 
 router.use('/session', sessionRouter);
@@ -21,6 +22,8 @@ router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 
 router.use('/spots', spotsRouter);
+
+router.use('/reviews', reviewRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const router = express.Router();
+
+// Import models used by router
+const { Spot, SpotImage, ReviewImage, User, Review } = require('../../db/models');
+
+// Import middleware used by router
+const { requireAuth } = require('../../utils/auth.js');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+// Middleware to validate the input for ********
+const validateNewSpot = [
+    check('address')
+        .exists({ checkFalsy: true })
+        .withMessage('Street address is required'),
+    check('city')
+        .exists({ checkFalsy: true })
+        .withMessage('City is required'),
+    check('state')
+        .exists({ checkFalsy: true })
+        .withMessage('State is required'),
+    check('country')
+        .exists({ checkFalsy: true })
+        .withMessage('Country is required'),
+    check('lat')
+        .exists({ checkFalsy: true })
+        .isDecimal({ force_decimal: true })
+        .withMessage('Latitude is not valid'),
+    check('lng')
+        .exists({ checkFalsy: true })
+        .isDecimal({ force_decimal: true })
+        .withMessage('Longitude is not valid'),
+    check('name')
+        .exists({ checkFalsy: true })
+        .isLength({ max: 50 })
+        .withMessage('Name must be less than 50 characters'),
+    check('description')
+        .exists({ checkFalsy: true })
+        .withMessage('Description is required'),
+    check('price')
+        .exists({ checkFalsy: true })
+        .withMessage('Price per day is required'),
+    handleValidationErrors
+];
+
+// Get all Reviews of the Current User
+router.get('/current', requireAuth, async (req, res, _next) => {
+    const { user } = req;
+
+    // If user is currently logged in, get userId of user
+    if (user) {
+        const safeUser = user.id;
+
+
+        // Get all reviews owned by the current user
+        const getOwnerReviews = await Review.findAll({
+            where: {
+                userId: safeUser
+            },
+            include: [
+                {
+                    model: User,
+                    attributes: {
+                        exclude: ['username', 'email', 'hashedPassword', 'createdAt', 'updatedAt'],
+                    }
+                },
+                {
+                    model: Spot,
+                    attributes: {
+                        exclude: ['description', 'createdAt', 'updatedAt'],
+                    }
+                },
+                {
+                    model: ReviewImage,
+                    attributes: {
+                        exclude: ['reviewId', 'createdAt', 'updatedAt']
+                    }
+                }
+            ],
+        });
+
+        let payload = [];
+
+        getOwnerReviews.forEach(element => {
+            let review = element.toJSON();
+            review.Spot.previewImage = "image url";
+            payload.push(review)
+        });
+
+        return res.json({
+            Reviews: payload
+        });
+    }
+});
+
+module.exports = router;
