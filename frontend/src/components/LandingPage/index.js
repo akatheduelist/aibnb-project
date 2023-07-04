68b3df3949900d4efb7c4a6e294bbdac04c1046e
Daniel Lewis <daniel.c.lewis@outlook.com>
2023-07-04 13:43:48 -0500

    Added hover tooltip, star rating, and onClick functionality to Landing Page per specs

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -1,10 +1,12 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { getAllSpots } from '../../store/spot';
 import testImg from '../../images/testspot.jpg';
 import './LandingPage.css';
 
 export default function LandingPage() {
+    const history = useHistory();
     const dispatch = useDispatch();
     const allSpots = Object.values(
         useSelector((state) => (state.spots.allSpots ? state.spots.allSpots : []))
@@ -15,18 +17,17 @@ export default function LandingPage() {
         dispatch(getAllSpots());
     }, [dispatch]);
 
-
     return (
         <>
             <div className="landing-page landing-page-container">
-                {allSpots?.map(({ name, city, state, avgRating, price, previewImage }) =>
+                {allSpots?.map(({ id, name, city, state, avgRating, price, previewImage }) =>
                     <>
-                        <div className="landing-page card-container">
+                        <div className="landing-page card-container" onClick={(e) => history.push(`/spots/${id}`)}>
                             <div>
-                                <img className="card-img" src={testImg} />
+                                <img className="card-img" src={testImg} title={name} alt={name} />
                             </div>
                             <div className="card-details">
-                                <span>{`${city}, ${state}`}</span><span className="avg-rating">{`${avgRating}`}</span>
+                                <span>{`${city}, ${state}`}</span><span className="avg-rating"><i class="fa-solid fa-star" />{` ${avgRating !== 'NaN' ? avgRating : 'New'}`}</span>
                             </div>
                             <div className="card-price">
                                 <span>{`$${price} night`}</span>
