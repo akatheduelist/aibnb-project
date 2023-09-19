dfcf4067d63fe5a93237ca871d916645e48f7a3f
Daniel Lewis <daniel.c.lewis@outlook.com>
2023-09-19 14:40:33 -0500

    Fixing issues issues with reveiew image seed info showing blank

--- backend/db/seeders/20230613164547-demo-reviewImage.js
+++ backend/db/seeders/20230613164547-demo-reviewImage.js
@@ -1,56 +1,32 @@
-'use strict';
+'use strict'
 
-let options = {};
+const { faker } = require('@faker-js/faker')
+const { Review } = require('../models')
+
+let options = {}
 if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
+  options.schema = process.env.SCHEMA // define your schema in options object
 }
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    options.tableName = 'ReviewImages';
-    await queryInterface.bulkInsert(options, [
-      {
-        reviewId: 1,
-        url: "image url"
-      },
-      {
-        reviewId: 2,
-        url: "image url"
-      },
-      {
-        reviewId: 3,
-        url: "image url"
-      },
-      {
-        reviewId: 4,
-        url: "image url"
-      },
-      {
-        reviewId: 5,
-        url: "image url"
-      },
-      {
-        reviewId: 6,
-        url: "image url"
-      },
-      {
-        reviewId: 7,
-        url: "image url"
-      },
-      {
-        reviewId: 8,
-        url: "image url"
-      },
-      {
-        reviewId: 9,
-        url: "image url"
-      }
-    ], {});
+  async up (queryInterface, Sequelize) {
+    options.tableName = 'ReviewImages'
+    const ids = await Review.findAll({
+      attributes: ['id']
+    })
+    let fakeReviewImages = []
+    for (let i = 0; i < 40; i++) {
+      fakeReviewImages.push({
+        reviewId: faker.number.int({ min: 1, max: ids.length }),
+        url: faker.image.urlLoremFlickr({ category: 'vacation' })
+      })
+    }
+    await queryInterface.bulkInsert(options, fakeReviewImages, {})
   },
 
-  async down(queryInterface, Sequelize) {
-    options.tableName = 'ReviewImages';
-    await queryInterface.bulkDelete(options);
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'ReviewImages'
+    await queryInterface.bulkDelete(options)
   }
-};
+}
